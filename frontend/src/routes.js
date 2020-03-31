63b92f94ba47a179ac1b55eb399c38f48f6499dd
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2020-03-31 17:18:00 -0300

    page profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import ProFile from './pages/Profile';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/register" component={Register}/>
+                <Route path="/profile" component={ProFile}/>
                 <Route path="/" /*exact*/ component={Logon} />
             </Switch>
         </BrowserRouter>
